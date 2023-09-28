bff3772dfecf6fe78e376f1b6d9c9c7638873fc9
Bojken07 <bojken.zaimi@stud.sti.se>
2023-09-28 10:07:25 +0200

    Lektion_6

---
+++ src/com/bojken/demo/Main.java
@@ -0,0 +1,25 @@
+package com.bojken.demo;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //for  (int i = 0; i < 5; i++) {
+          //  System.out.println("index: " + i);
+        //}
+
+        int[] numbers = {0, 100, 1000};
+        int test;
+
+        System.out.println(test);
+
+       // Scanner scanner = new Scanner(System.in);
+       // System.out.println("Typ something to  crash the program");
+       // scanner.nextLine();
+
+       // for (int i = 0; i < numbers.length; i++) {
+         //   System.out.println(numbers[i]);
+       // }
+    }
+}
